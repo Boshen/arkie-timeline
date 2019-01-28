32ddec3c7f1cdc177bca5adc642c54172a228e36
Boshen <boshenc@gmail.com>
2019-01-28 10:59:34 +0800

    add Typography.js

---
+++ src/utils/typography.js
@@ -0,0 +1,9 @@
+// https://kyleamathews.github.io/typography.js/
+import Typography from "typography"
+
+const typography = new Typography({
+  baseFontSize: '18px',
+  baseLineHeight: 1.5,
+})
+
+export default typography
