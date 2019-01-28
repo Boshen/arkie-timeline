37adc582ac6b600b5b85ff31d44bcdb961fe4f56
Boshen <boshenc@gmail.com>
2019-01-28 17:54:36 +0800

    add image

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,8 @@ import React from 'react'
 
 import Layout from '../components/layout'
 import Chart from '../components/chart'
+import Image, { astronaut } from '../components/image'
+
 import './index.css'
 
 const IndexPage = () => (
@@ -17,7 +19,7 @@ const IndexPage = () => (
       <h1 className='event'><div className='event-bullet' />有的</h1>
       <h3 className='time'>2016.07 - 2016.10</h3>
       <h3 className='intro'>「有的」是 arkie 的前身，当时产品名字还不叫 arkie. 第一版的产品形态非常简单，只有一个编辑器和我的作品，可以创建、编辑和保存海报。后来逐步加入设计算法，从纯手动做设计往自动生成设计转变。</h3>
-      <image />
+      <Image query={astronaut}/>
       <video />
     </Layout>
 
