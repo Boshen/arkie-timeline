0a845f4fda0b53331904f64e44bf4f7e36289a85
Boshen <boshenc@gmail.com>
2019-01-29 17:54:22 +0800

    clean up

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,44 +1,32 @@
 // https://kyleamathews.github.io/typography.js/
 import Typography from 'typography'
 
+const fontFamily = [
+    'Chinese Quote',
+    '-apple-system',
+    'BlinkMacSystemFont',
+    'Segoe UI',
+    'PingFang SC',
+    'Hiragino Sans GB',
+    'Microsoft YaHei',
+    'Helvetica Neue',
+    'Helvetica, Arial',
+    'sans-serif',
+    'Apple Color Emoji',
+    'Segoe UI Emoji',
+    'Segoe UI Symbol',
+]
+
 const typography = new Typography({
   baseFontSize: '14px',
   baseLineHeight: 1.6,
   headerLineHeight: 1.6,
-  headerFontFamily: [
-    'Chinese Quote',
-    '-apple-system',
-    'BlinkMacSystemFont',
-    'Segoe UI',
-    'PingFang SC',
-    'Hiragino Sans GB',
-    'Microsoft YaHei',
-    'Helvetica Neue',
-    'Helvetica, Arial',
-    'sans-serif',
-    'Apple Color Emoji',
-    'Segoe UI Emoji',
-    'Segoe UI Symbol',
-  ],
-  bodyFontFamily: [
-    'Chinese Quote',
-    '-apple-system',
-    'BlinkMacSystemFont',
-    'Segoe UI',
-    'PingFang SC',
-    'Hiragino Sans GB',
-    'Microsoft YaHei',
-    'Helvetica Neue',
-    'Helvetica, Arial',
-    'sans-serif',
-    'Apple Color Emoji',
-    'Segoe UI Emoji',
-    'Segoe UI Symbol',
-  ],
+  headerFontFamily: fontFamily,
+  bodyFontFamily: fontFamily,
   overrideStyles: () => ({
     h1: {
       color: 'white',
-      fontSize: '100px',
+      fontSize: '5em',
       fontWeight: 600,
       textTransform: 'uppercase',
     },
